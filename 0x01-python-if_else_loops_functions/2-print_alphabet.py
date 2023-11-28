6c8b12c8d7a0abe2948c171aae068ad39bd5e438
Clinton-okumu <clintonomondiokumu@gmail.com>
2023-11-28 23:19:23 +0300

    changes

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 for character in range(97, 123):
-    print("{:c}".format(character), end='')
+    print("{:c}".format(character), end="")
 print("guillaume")
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for number in range(100):
-    print(str(number).zfill(2), end=' ')
-    if number < 99:
-        print(',', end='')
+for num in range(00, 100):
+    print("{:02d}".format(num), end='\n' if num == 99 else ", ")
