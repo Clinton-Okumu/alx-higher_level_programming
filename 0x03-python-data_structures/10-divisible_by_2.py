285497785e9b5783a0aa8d93edfb193c070eb081
Clinton-okumu <clintonomondiokumu@gmail.com>
2023-12-04 14:07:59 +0300

    10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,3 +1,10 @@
-name = input ('What is your first name?')
-distance_in_km = input ('Distance you have covered in km ?')
-distance_in_miles = distance_in_km * 1.609
+#!/usr/bin/python3
+divisible_by_2 = __import__('10-divisible_by_2').divisible_by_2
+
+my_list = [0, 1, 2, 3, 4, 5, 6]
+list_result = divisible_by_2(my_list)
+
+i = 0
+while i < len(list_result):
+    print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
+    i += 1
