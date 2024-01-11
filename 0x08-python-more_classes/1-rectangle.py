bb24fb4bb64da52a8b92d9ac12cd038ed7c877e1
Clinton-okumu <clintonomondi@gmail.com>
2024-01-11 08:37:01 +0300

    classes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,40 +1,41 @@
 #!/usr/bin/python3
+"""class that defines a rectangle"""
+
 
 class Rectangle:
-    """Represent a rectangle."""
-
     def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle.
+        """private instance attributes
+        with leading double underscore"""
+        self.__width = width
+        self.__height = height
 
-        Args:
-            width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle.
-        """
-        self.width = width
-        self.height = height
-
+    """property to retrieve width"""
     @property
     def width(self):
-        """Get/set the width of the rectangle."""
         return self.__width
-
+    
     @width.setter
     def width(self, value):
+        """setter for the width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-
+    
     @property
     def height(self):
-        """Get/set the height of the rectangle."""
+        """function defining private property"""
         return self.__height
-
+    
     @height.setter
     def height(self, value):
+        """function defining private property"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def __str__(self):
+        return f"{{'_Rectangle__height': {self.__height}, '_Rectangle__width': {self.__width}}}"
